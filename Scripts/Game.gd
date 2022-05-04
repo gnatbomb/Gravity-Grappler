721fc6851b401f85b69617d92f61c1d6efe7c510
Natalie <nbombard@ualberta.ca>
2022-05-03 19:44:31 -0600

    added gravity wells

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -4,16 +4,22 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var width = 1600
-var height = 900
+var width
+var height
+var wellradius = 256 / 5 / 2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	width = get_viewport().size.x
+	height = get_viewport().size.y
 	new_game()
 
 func new_game():
+	
 	$Player.start($StartPosition.position)
+	$Gravity_Well_1.start(Vector2(width / 4, height / 2))
+	$Gravity_Well_2.start(Vector2(width * 3 / 4, height / 2))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
